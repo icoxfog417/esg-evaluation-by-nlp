ee281899a9cb5ef443387a8c54882c39e15208ec
icoxfog417 <icoxfog417@yahoo.co.jp>
2020-01-10 21:37:13 +0900

    update pdf reader and notebook

--- evaluator/data/pdf_reader.py
+++ evaluator/data/pdf_reader.py
@@ -1,162 +1,115 @@
-import os
-import time
-from functools import wraps, partial
 import unicodedata
-import docker
-import requests
-import tika
-import pandas as pd
+from io import StringIO
+from pdfminer.high_level import extract_text, extract_text_to_fp
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import HTMLConverter, TextConverter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LAParams
+from tqdm import tqdm
 from bs4 import BeautifulSoup
-from tika import parser
-
-
-def unset_proxy(func):
-
-    @wraps(func)
-    def _unset_proxy(*args, **kwargs):
-        encoding = os.getenv("PYTHONIOENCODING", "")
-        http_proxy = os.getenv("HTTP_PROXY", "")
-        https_proxy = os.getenv("HTTPS_PROXY", "")
-
-        os.environ["PYTHONIOENCODING"] = "utf8"
-        os.environ["HTTP_PROXY"] = ""
-        os.environ["HTTPS_PROXY"] = ""
-
-        result = func(*args, **kwargs)
-
-        os.environ["PYTHONIOENCODING"] = encoding
-        os.environ["HTTP_PROXY"] = http_proxy
-        os.environ["HTTPS_PROXY"] = https_proxy
-        return result
-
-    return _unset_proxy
-
-
-def wait_docker_up(host, interval):
-
-    def _wait_docker_up(_host, _interval, func):
-
-        @wraps(func)
-        def __wait_docker_up(*args, **kwargs):
-            result = func(*args, **kwargs)
-            is_up = False
-            while not is_up:
-                try:
-                    resp = requests.get(_host,
-                                        proxies={
-                                            "http": None,
-                                            "https": None
-                                        })
-                    if resp.ok:
-                        is_up = True
-                except Exception as ex:
-                    pass
-                time.sleep(_interval)
-
-            return result
-
-        return __wait_docker_up
-
-    return partial(partial(_wait_docker_up, host), interval)
+import pandas as pd
 
 
 class PDFReader():
-    _CONTAINER = None
 
-    @wait_docker_up(host="http://localhost:9998", interval=1)
     def __init__(self):
-        self.client = docker.from_env()
-        if PDFReader._CONTAINER is None:
-            PDFReader._CONTAINER = self.client.containers.run(
-                "logicalspark/docker-tikaserver:latest",
-                ports={"9998/tcp": "9998"},
-                remove=True, detach=True)
+        pass
 
-    @unset_proxy
-    def read(self, path, xml_content=False):
-        parsed = parser.from_file(path, xmlContent=xml_content)
-        return Content(parsed)
+    def read(self, path, html=False):
+        text = StringIO()
+        if html:
+            with open(path, "rb") as f:
+                extract_text_to_fp(f, text, laparams=LAParams(),
+                                   output_type="html", codec=None)
+            text = text.getvalue()
+        else:
+            text = extract_text(path)
+        return text
 
-    def normalize(self, text):
-        if text is None:
-            return ""
-        _text = text.replace("\r", "").replace("\n", " ")
-        _text = _text.strip()
-        _text = unicodedata.normalize("NFKC", _text)
-        return _text
+    def read_pages(self, path, html=False,
+                   laparams=None, maxpages=0, page_numbers=None,
+                   password="", scale=1.0, rotation=0, layoutmode='normal',
+                   output_dir=None, strip_control=False, debug=False,
+                   disable_caching=False, **kwargs):
 
-    @unset_proxy
+        rsrcmgr = PDFResourceManager(caching=True)
+        pages = []
+        with open(path, "rb") as f:
+            for page in PDFPage.get_pages(f, None, maxpages=0,
+                                          check_extractable=True):
+                page.rotate = (page.rotate + rotation) % 360
+                text = StringIO()
+                if html:
+                    device = HTMLConverter(rsrcmgr, text, codec=None, scale=scale,
+                                           layoutmode=layoutmode, laparams=laparams)
+                else:
+                    device = TextConverter(rsrcmgr, text, codec=None, laparams=laparams)
+
+                interpreter = PDFPageInterpreter(rsrcmgr, device)
+                interpreter.process_page(page)
+                pages.append(text.getvalue())
+                device.close()
+
+        return pages
+
     def read_to_frame(self, path):
-        parsed = parser.from_file(path, xmlContent=True)
-        xml = BeautifulSoup(parsed["content"], "lxml-xml")
-        body = xml.find("body")
         items = []
-        for p, page in enumerate(body.find_all("div")):
-            page_index = p + 1
-            for c, content in enumerate(page.find_all("p")):
-                content_index = c + 1
+        pages = self.read_pages(path, html=True)
+        page_index = 0
+        for i, p in enumerate(pages):
+            html = BeautifulSoup(p, "html.parser")
+            if not html:
+                continue
+            page = html.get_text("\n")
+            contents = page.split("\n\n")
+            content_index = 0
+            for j, s in enumerate(contents):
+                c = s.strip()
+                if not c:
+                    continue
+
                 item = {
                     "page": page_index,
                     "order": content_index,
-                    "content": content.text.strip()
+                    "content": c
                 }
                 items.append(item)
+                content_index += 1
+
+            page_index += 1
 
         df = pd.DataFrame(items)
         return df
 
+    def normalize(self, text):
+        if text is None:
+            return ""
+        _text = text.replace("\r", "").replace("\n", "").strip()
+        _text = unicodedata.normalize("NFKC", _text)
+        return _text
+
     def preprocess_frame(self, df, lower=True):
-        prefixes = []
-        suffixes = []
+        repeat_check = []
         preprocessed = []
-        magic_number = 10
+        magic_number = 30
 
         for i, row in df.iterrows():
             content = self.normalize(row["content"])
             if lower:
                 content = content.lower()
             if len(content) < magic_number:
-                continue
-            else:
-                length = int(magic_number / 2)
-                prefix = content[:length]
-                suffix = content[-length:]
-                if prefix in prefixes:
+                if content in repeat_check:
                     continue
                 else:
-                    prefixes.append(prefix)
+                    repeat_check.append(magic_number)
 
-                if suffix in suffixes:
-                    continue
-                else:
-                    suffixes.append(suffix)
+            item = {}
+            for c in df.columns:
+                item[c] = row[c]
+                if c == "content":
+                    item[c] = content
 
-                item = {}
-                for c in df.columns:
-                    item[c] = row[c]
-                    if c == "content":
-                        item[c] = content
-
-                preprocessed.append(item)
+            preprocessed.append(item)
 
         df = pd.DataFrame(preprocessed)
         return df
-
-    @classmethod
-    def stop(cls):
-        if cls._CONTAINER:
-            cls._CONTAINER.stop()
-
-
-class Content():
-
-    def __init__(self, parsed):
-        self._parsed = parsed
-
-    @property
-    def metadata(self):
-        return self._parsed["metadata"]
-
-    @property
-    def content(self):
-        return self._parsed["content"]
--- tests/data/test_pdf_reader.py
+++ tests/data/test_pdf_reader.py
@@ -6,22 +6,17 @@ from evaluator.data.pdf_reader import PDFReader
 
 class TestPDFReader(unittest.TestCase):
 
-    @classmethod
-    def tearDownClass(cls):
-        PDFReader.stop()
-
     def test_read_pdf_text(self):
         path = os.path.join(os.path.dirname(__file__), "../_data")
         storage = Storage(path)
         url = "https://global.toyota/pages/global_toyota/ir/library/annual/2019_001_annual_jp.pdf"
         file_path = storage.download(url, "./test_pdf.pdf")
         reader = PDFReader()
-        content = reader.read(file_path)
-        self.assertTrue(content.metadata)
-        self.assertTrue(content.content)
-        self.assertEqual(content.metadata["Author"],
-                         "TOYOTA MOTOR CORPORATION")
-        self.assertTrue("アニュアルレポート2019", content.content[:200])
+        text = reader.read(file_path, html=True)
+        self.assertEqual(text[:6], "<html>")
+
+        text = reader.read(file_path)
+        self.assertTrue("目次" in text[:6])
         os.remove(file_path)
 
     def test_read_pdf_as_frame(self):
@@ -31,5 +26,17 @@ class TestPDFReader(unittest.TestCase):
         file_path = storage.download(url, "./test_pdf.pdf")
         reader = PDFReader()
         df = reader.read_to_frame(file_path)
+        self.assertGreater(len(df), 1)
+        os.remove(file_path)
+
+    def test_preprocess(self):
+        path = os.path.join(os.path.dirname(__file__), "../_data")
+        storage = Storage(path)
+        url = "https://global.toyota/pages/global_toyota/ir/library/annual/2019_001_annual_en.pdf"
+        file_path = storage.download(url, "./test_pdf_preprocess.pdf")
+        reader = PDFReader()
+        df = reader.read_to_frame(file_path)
+        df = reader.preprocess_frame(df)
+        df.to_csv("sample.csv", index=False)
         self.assertGreater(len(df), 1)
         os.remove(file_path)
