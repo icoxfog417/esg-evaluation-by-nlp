c000246416d327f9d73c279c34578cf1c747e0a9
icoxfog417 <icoxfog417@yahoo.co.jp>
2022-05-25 06:44:19 +0000

    Update Notebook

--- esg_nlp/model/encoder.py
+++ esg_nlp/model/encoder.py
@@ -5,23 +5,40 @@ from transformers.pipelines import FeatureExtractionPipeline
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def encode(pretrained_model_name, text, batch_size=10):
+class DistilBertFeatureExtractionPipeline(FeatureExtractionPipeline):
+    
+    def preprocess(self, inputs, truncation=True):
+        # Make truncation=True
+        # https://huggingface.co/docs/transformers/internal/tokenization_utils#transformers.PreTrainedTokenizerBase.__call__.truncation
+        return_tensors = self.framework
+        if truncation is None:
+            kwargs = {}
+        else:
+            kwargs = {"truncation": truncation}
+        # Refer the issue
+        # https://github.com/huggingface/transformers/issues/2702
+        model_inputs = self.tokenizer(inputs, return_token_type_ids=False,
+                                      return_tensors=return_tensors, **kwargs)
+        return model_inputs
+
+
+def encode(pretrained_model_name, text, batch_size=10, pretrained_tokenizer_name=""):
     print("Loading pretrained model...")
     model = AutoModel.from_pretrained(pretrained_model_name)
     print("Prepair the tokenizer...")
-    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
+    tokenizer_name = pretrained_tokenizer_name if pretrained_tokenizer_name else pretrained_model_name
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
     print("Set the pipeline.")
-    nlp = FeatureExtractionPipeline(model=model, tokenizer=tokenizer)
+    nlp = DistilBertFeatureExtractionPipeline(model=model, tokenizer=tokenizer)
     print("Inference start.")
     # shape: (batch_size, number_of_token, vector_size)
     features = None
     if isinstance(text, str):
-        vector = nlp(text)
+        vector = nlp(text, add_special_tokens=False)
         vector = np.array(vector)
-        # get [CLS] tokens (added by add_special_tokens)
-        # [CLS] tokens does not suitable for sentence representation?
-        # https://huggingface.co/transformers/model_doc/bert.html
-        # features = np.squeeze(vector[:, 0, :])
+        # Take mean of embedding not [CLS] tokens (added by add_special_tokens)
+        # https://www.ogis-ri.co.jp/otc/hiroba/technical/similar-document-search/part9.html
+        # features = np.squeeze(vector[:, 0, :])  # for CLS
         features = np.mean(vector, axis=1)
     else:
         features = []
@@ -32,6 +49,9 @@ def encode(pretrained_model_name, text, batch_size=10):
             batch = _text[i:i + batch_size]
             vector = nlp(batch, add_special_tokens=False)
             feature = np.array([np.mean(v[0], axis=0) for v in vector])
+            # for CLS token
+            #vector = nlp(batch, add_special_tokens=True)
+            #feature = np.array([v[0][0] for v in vector])
             features.append(feature)
 
         features = np.vstack(features)
@@ -41,12 +61,14 @@ def encode(pretrained_model_name, text, batch_size=10):
 
 
 if __name__ == "__main__":
-    feature = encode("cl-tohoku/bert-base-japanese-whole-word-masking",
+    feature = encode("bandainamco-mirai/distilbert-base-japanese",
                     ["スーパーでリンゴを買ってきた。",
                      "明日は晴れだと思うので、スーパーに行きたい。",
                      "おいしそうなリンゴを買ってきた。",
                      "私は歌手です。",
-                     "焼き肉は石川さんが詳しい。"], batch_size=2)
+                     "焼き肉は石川さんが詳しい。"],
+                     batch_size=2,
+                     pretrained_tokenizer_name="cl-tohoku/bert-base-japanese-whole-word-masking")
 
     print(feature.shape)
     print(np.max(feature, axis=1))
